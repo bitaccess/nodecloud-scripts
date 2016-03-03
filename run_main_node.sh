37e669b0517347f2fe73ffc0fd66fad6ab2070fa
Moe Adham <moe@bitaccess.co>
2016-03-03 17:29:54 -0500

    [docker] docker + firewall = pain

--- run_main_node.sh
+++ run_main_node.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --volumes-from=bitcoin-data1 -v /home/node/mainnet-conf/bitcoin.conf:/bitcoin/bitcoin.conf  -p 127.0.0.1:3001:3001 -p 127.0.0.1:8333:8333 bitcorenode1
+sudo docker run --volumes-from=bitcoin-data1 -v /home/node/mainnet-conf/bitcoin.conf:/bitcoin/bitcoin.conf  -p 3001:3001 -p 8333:8333 bitcorenode1
--- run_test_node.sh
+++ run_test_node.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --volumes-from=testcoin-data1 -v /home/node/testnet-conf/bitcoin.conf:/bitcoin/bitcoin.conf  -p 127.0.0.1:3000:3000 -p 127.0.0.1:18333:18333 testcorenode1
+sudo docker run --volumes-from=testcoin-data1 -v /home/node/testnet-conf/bitcoin.conf:/bitcoin/bitcoin.conf  -p 3000:3000 -p 18333:18333 testcorenode1
